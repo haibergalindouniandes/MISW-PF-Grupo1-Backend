ef4815d5023ccc6696bec02613ae093495a65a60
jhon.caicedo <jhon.caicedo@rappi.com>
2024-04-11 23:23:29 -0500

    se agrega archivo sh test

--- gestor_usuarios/test_gestor_usuarios.sh
+++ gestor_usuarios/test_gestor_usuarios.sh
@@ -7,8 +7,6 @@ mkdir -p $TEST_PATH
 echo "Se realiza la creación del directorio [$TEST_PATH]"
 MIN_COVERAGE=80
 echo "La cobertura minima establecida para pasar las pruebas es [$MIN_COVERAGE]"
-COVERAGE_FILE=usuarios-coverage-results.txt
-echo "El archivo con los resultados de las pruebas de cobertura es [$COVERAGE_FILE]"
 echo "<================== [Fin] Configuración ==================>"
 echo "<================== [Inicio] instalacion de dependencias ==================>"
 pwd
@@ -18,7 +16,6 @@ echo "<================== [Inicio] Ejecucion test ==================>"
 pwd
 cd /workspace/gestor_usuarios
 git checkout feature_gestor_usuarios
+git pull
 pytest --cov-fail-under=80 --cov=src --cov-report=html:cov_report
-
-echo "Se mueve directorio [$COVERAGE_PATH] a [$TEST_PATH/$COVERAGE_PATH]"
 echo "<================== [Fin][Exitoso] Ejecucion test ==================>"
