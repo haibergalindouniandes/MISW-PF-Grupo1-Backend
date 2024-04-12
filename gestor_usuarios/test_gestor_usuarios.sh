59213c3ad5fb81c95099a9a04d48b193b9e7cd00
jhon.caicedo <jhon.caicedo@rappi.com>
2024-04-11 20:11:25 -0500

    se agrega archivo sh test

---
+++ gestor_usuarios/test_gestor_usuarios.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+####################################################################################################################################
+echo "<================== [Inicio] Configuración ==================>"
+TEST_PATH=tests-results-usuarios
+mkdir -p $TEST_PATH
+echo "Se realiza la creación del directorio [$TEST_PATH]"
+MIN_COVERAGE=80
+echo "La cobertura minima establecida para pasar las pruebas es [$MIN_COVERAGE]"
+echo "<================== [Fin] Configuración ==================>"
+echo "<================== [Inicio] instalacion de dependencias ==================>"
+pwd
+pip install -r requirements.txt
+
+echo "<================== [Fin][Exitoso] instalacion dependencias ==================>"
+echo "<================== [Inicio] Ejecucion test ==================>"
+pytest --cov-fail-under=80 --cov=src --cov-report=html:cov_report --junitxml=${SHORT_SHA}_usuarios__test_log.xml
+echo "<================== [Fin][Exitoso] Ejecucion test ==================>"
