31e5d914a918267aff1f711c9044fdb7fabe2298
jhon.caicedo <jhon.caicedo@rappi.com>
2024-04-12 20:17:58 -0500

    Se modifican regex de validacion de usuario

--- gestor_usuarios/test_gestor_usuarios.sh
+++ gestor_usuarios/test_gestor_usuarios.sh
@@ -8,33 +8,36 @@ echo "Se realiza la creación del directorio [$TEST_PATH]"
 MIN_COVERAGE=80
 echo "La cobertura minima establecida para pasar las pruebas es [$MIN_COVERAGE]"
 echo "<================== [Fin] Configuración ==================>"
-echo "<================== [Inicio] instalacion de dependencias ==================>"
+
+
+echo "<================== [Inicio] Actualizacion de codigo ==================>"
 pwd
 cd /workspace/gestor_usuarios
 pwd
-pip install virtualenv
-python3 -m venv gestor_usuarios
-source gestor_usuarios/bin/activate
-#python3 -m venv .venv
-#source .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip --version
 python3 -m pip install -r requirements.txt
-echo "<================== [Fin][Exitoso] instalacion dependencias ==================>"
-echo "<================== [Inicio] Ejecucion test ==================>"
-#pwd
-#cd /workspace/gestor_usuarios
+echo "<================== [Fin][Exitoso] Actualizacion de Codigo ==================>"
 pwd
 git pull
 git checkout feature_gestor_usuarios
 git pull
-echo "Se inicia ejecucion de pruebas"
+echo "<================== [Inicio] Ejecucion test ==================>"
+#pwd
+#cd /workspace/gestor_usuarios
+pwd
+cd /workspace/gestor_usuarios
+pwd
+python3 -m pip install --upgrade pip
+python3 -m pip --version
+python3 -m pip install -r requirements.txt
 #pip install pytest
 #pip install pytest-cov
 #pip install pytest-env
 #pip install python-dotenv
 #pip install flask
 #pip install flask_restful
+echo "Se inicia ejecucion de pruebas"
 pytest --cov-fail-under=80 --cov=src --cov-report=html:cov_report
 echo "fin ejecucion de pruebas"
 echo "<================== [Fin][Exitoso] Ejecucion test ==================>"
