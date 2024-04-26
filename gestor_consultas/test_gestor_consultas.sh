02f4a74ae6651f9547691f2dcc7fc509587a3341
Jorge M. Carrillo <jm.carrillo@uniandes.edu.co>
2024-04-25 22:44:54 -0500

    configuracion para CI/CD

--- gestor_consultas/test_gestor_consultas.sh
+++ gestor_consultas/test_gestor_consultas.sh
@@ -2,7 +2,7 @@
 
 ####################################################################################################################################
 echo "<================== [Inicio] Configuración ==================>"
-TEST_PATH=tests-results-entrenamientos
+TEST_PATH=tests-results-consultas
 mkdir -p $TEST_PATH
 echo "Se realiza la creación del directorio [$TEST_PATH]"
 MIN_COVERAGE=80
@@ -10,14 +10,14 @@ echo "La cobertura minima establecida para pasar las pruebas es [$MIN_COVERAGE]"
 echo "<================== [Fin] Configuración ==================>"
 echo "<================== [Inicio] instalacion de dependencias ==================>"
 pwd
-pip install -r gestor_entrenamientos/requirements.txt
+pip install -r gestor_consultas/requirements.txt
 echo "<================== [Fin][Exitoso] instalacion dependencias ==================>"
 echo "<================== [Inicio] Ejecucion test ==================>"
 pwd
-cd /workspace/gestor_entrenamientos
+cd /workspace/gestor_consultas
 pwd
 git pull
-git checkout feature_gestor_entrenamientos
+git checkout feature_gestor_consultas
 git pull
 pytest --cov-fail-under=80 --cov=src --cov-report=html:cov_report
 echo "<================== [Fin][Exitoso] Ejecucion test ==================>"
